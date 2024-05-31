2eacd2192fdddea88c424c9e35b169909261dee5
+ <+>
2024-05-31 23:51:09 +0300

    creating objects using object literals

---
+++ index.js
@@ -0,0 +1,13 @@
+// Object Literals
+const circe = {
+    radius: 5,
+    locatio: {
+        x: 1,
+        y: 1
+    },
+    draw: function() {
+        console.log("draw")
+    }
+};
+
+circe.draw();
