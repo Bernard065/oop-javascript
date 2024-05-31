fc8479217614461594d8c8d568c501081328b08c
+ <+>
2024-06-01 00:06:02 +0300

    using cunstructor

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // Object Literals
 const circe = {
     radius: 5,
-    locatio: {
-        x: 1,
-        y: 1
-    },
     draw: function() {
         console.log("draw")
     }
 };
 
 circe.draw();
+
+// Using constructor
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log("draw");
+    }
+}
+
+const circle = new Circle();
